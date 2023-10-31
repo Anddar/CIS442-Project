1f40bf5fcb30f63f409ce28f4e2125c28886a535
BuildTools <unconfigured@null.spigotmc.org>
2023-10-31 00:24:50 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+**CIS 442 Project**
+
+Requirements:
+    - Users should use Python 3.10 or greater to run the code of this project.
+    - User must run the main.py program to execute and find masqueraded files.
+
+
+Setup Requirements:
+
+    Clone project repo down to local computer
+
+    Using terminal with Python create a local python enviroment in the repo folder:
+        
+        Step 1: Create environment (maybe python or python3 in your terminal)
+            Mac/Linux/Windows: python -m venv .env
+                               python3 -m venv .env
+
+        Step 2: Activate the python venv
+            Windows: call .\.env\Scripts\activate
+
+            Mac/Linux: source ./.env/bin/activate
+
+    Now that venv is active use Python to execute main.py
+        python main.py
+        python3 main.py
+
+    At this point the GUI should pop up from running the main.py
+
+
+Using the GUI:
+
+    - You may type in a directory in the top box, this could be a absolute path or a relative path to 
+      the main.py file you are running or where ever your current working directory is.
+        -> If a dirctory is invalid the program will notify you that it is invalid in the output box at bottom of GUI
+           after pressing the "Run" button
+    
+    - There is also a "Browse Directory" button this will allow you to select a directory from your
+      file explorer GUI, this could be (Finder (Mac), File Explorer (Windows), etc...)
+
+    - Once your directory is input or selected from "Browse Dirctory" then you may press the "Run" button,
+      this will search within that directory identifying files that are masqueraded printing them to the 
+      output box at the bottom of the GUI
+        -> This output box is scrollable if there are many masqueraded files
---
+++ main.py
@@ -0,0 +1,121 @@
+# Written By: Andrew Raposo
+# Date: 2023-10-23
+# Description: This program is a masquaraded file finder. It takes in a directory and searches for
+#             masquaraded files in that directory. It does this by comparing the file signature of
+#             the file to the file signature that corresponds to the file extension of the file.
+#             If it does not match then we have a masquaraded file.
+
+# Reference: This project was given by the University of Massachusetts Dartmouth for the course 
+# CIS 442 - Digital Forensics taught by Professor Gohkan Kul.
+
+import os, tkinter as tk
+from tkinter import filedialog, scrolledtext
+from signatures import file_signatures
+
+# Determines if path is real meaning and existing directory
+def check_directory(directory):
+    if str(directory).strip() == "":
+        clear_output()
+        output_text.insert(tk.END, "Directory field is empty.\n")
+        return False
+    
+    elif not os.path.isdir(directory):
+        clear_output()
+        output_text.insert(tk.END, f"Directory does not exist: {directory}\n")
+        return False
+    
+    return True
+
+# Matches file signature to files in a directory given, and finds masquaraded files in that directory.
+def match_file_signatures(dir_path):
+    for _file in os.listdir(dir_path):
+        if os.path.isdir(_file):
+            continue
+
+        file_path = os.path.abspath(os.path.join(dir_path, _file))
+        file_extension = _file.split(".")[-1]
+        
+        if file_extension not in file_signatures:
+            output_text.insert(tk.END, f"File extension not supported: {file_extension}, file path: {file_path}\n")
+            continue
+
+        file_signature = file_signatures[file_extension]
+        found_signature_match = False
+
+        for signature in file_signature:
+            with open(file_path, "rb") as f:
+                file_contents = f.read(len(signature) if "?" not in signature else int(len(signature)/2))
+
+            if file_contents == signature:
+                found_signature_match = True
+                break
+            elif "?" in signature:
+                part1_signature, part2_signature = bytes.fromhex(signature.split("?")[0]), bytes.fromhex(signature.split("?")[-1])
+                if file_contents[:len(part1_signature)] == part1_signature and file_contents[-len(part2_signature):] == part2_signature:
+                    found_signature_match = True
+                    break
+
+        if not found_signature_match:
+            # Print the masquaraded file to the output box
+            output_text.insert(tk.END, f"Masquaraded file found: {file_path}, contents:  {file_contents}\n")
+
+# Opens the file system search to allow the user to select a directory
+def open_file_dialog():
+    directory = filedialog.askdirectory()
+    directory_entry.delete(0, tk.END)
+    directory_entry.insert(0, directory)
+
+# Submits the directory that the user has entered to find masquaraded files in
+def submit_action():
+    # Get the input directory
+    directory = directory_entry.get()
+
+    # Check if directory is valid
+    if not check_directory(directory):
+        return
+
+    # Clear the output box
+    clear_output()
+
+    # Start searching for masquaraded files
+    output_text.insert(tk.END, f"Processing directory: {directory}\n")
+    match_file_signatures(directory)
+
+# Clears the output box in the GUI
+def clear_output():
+    output_text.delete('1.0', tk.END)
+
+def main():
+    # Start the GUI
+    window.mainloop()
+
+    return 0
+
+if __name__ == '__main__':
+    # Create the main window
+    window = tk.Tk()
+    window.title("Masquaraded File Finder")
+
+    # Configure the window to be resizable
+    window.geometry("600x400") 
+    window.resizable(width=True, height=True) 
+
+    # Directory Input
+    directory_label = tk.Label(window, text="Enter Directory:")
+    directory_label.pack()
+    directory_entry = tk.Entry(window, width=65)
+    directory_entry.pack()
+
+    # Browse Button
+    browse_button = tk.Button(window, text="Browse Directory", command=open_file_dialog)
+    browse_button.pack()
+
+    # Submit Button
+    submit_button = tk.Button(window, text="Run", command=submit_action)
+    submit_button.pack()
+
+    # Output Box
+    output_text = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=80, height=20)
+    output_text.pack()
+
+    main()
